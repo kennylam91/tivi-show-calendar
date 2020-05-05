6112e6f1d69a0d325646183dc51c0189e87efce5
pham van lam <phamvanlam1991@gmail.com>
2020-05-05 23:29:17 +0700

    done session

--- components/programs/Program.vue
+++ components/programs/Program.vue
@@ -12,7 +12,7 @@
           :alt="program.name"
         >
       </el-link>
-      <div v-else>{{ program.name }}</div>
+      <div v-else class="mb-4">{{ program.name }}</div>
       <div
         class="color-info smaller-font-size my-2 shorten-text"
       >
@@ -36,22 +36,21 @@
         </el-tooltip>
       </el-link>
 
-      <div v-if="live" class="small-font-size mb-1 bold" style="color: #000000b5;">
-        <div>{{ COMMON.CHANNEL }}: {{ program.schedule.channelName }}</div>
-        <div>{{ COMMON.TIME }}: {{ parseTime(program.schedule.startTime.seconds) }}-{{ parseTime(program.schedule.endTime.seconds) }}</div>
-      </div>
-
-      <div v-if="!live">
+      <div>
         <el-tag
           v-for="(item, index) in getCategoryList(program.categories)"
           :key="index"
           size="small"
           effect="dark"
-          type="info"
-          style="margin: 2px; padding: 0 4px;"
+          :type="categoryMap.get(item)"
+          style="margin: 2px; padding: 0 0px; width: 63px;"
         >
           {{ item | getCategory }}
         </el-tag>
+      </div>
+      <div v-if="live" class="small-font-size mb-1 bold" style="color: #000000b5;">
+        <div>{{ COMMON.CHANNEL }}: {{ program.schedule.channelName }}</div>
+        <div>{{ COMMON.TIME }}: {{ parseTime(program.schedule.startTime.seconds) }}-{{ parseTime(program.schedule.endTime.seconds) }}</div>
       </div>
     </el-card>
   </div>
@@ -69,6 +68,34 @@ export default {
       required: false,
       type: Boolean,
       default: () => false
+    }
+  },
+  data() {
+    return {
+      categoryMap: new Map([
+        [1, 'primary'], // 'Phim',
+        [2, 'primary'], // 'Game show',
+        [3, 'primary'], // 'Thực tế',
+        [4, 'primary'], // 'Trực tiếp',
+        [5, 'info'], // 'Phóng sự',
+        [6, 'info'], // 'Tin tức',
+        [7, 'info'], // 'Khoa học',
+        [8, 'info'], // 'Tài liệu',
+        [9, 'primary'], // 'Khám phá',
+        [10, 'primary'], // 'Ca nhạc',
+        [11, 'danger'], // 'Kinh dị',
+        [12, 'success'], // 'Hài hước',
+        [13, 'danger'], // 'Hành động',
+        [14, 'warning'], // 'Viễn tưởng',
+        [15, 'warning'], // 'Thần thoại',
+        [16, 'danger'], // 'Hình sự',
+        [17, 'success'], // 'Phiên lưu',
+        [18, 'warning'], // 'Cổ trang',
+        [19, 'danger'], // 'Chiến tranh',
+        [20, 'warning'], // 'Hoạt hình',
+        [21, 'success'], // 'Tình cảm',
+        [22, 'success'] // 'Tâm lý',
+      ])
     }
   },
   computed: {
@@ -91,6 +118,11 @@ export default {
     parseTime(time) {
       return parseVNTime(time, '{H}:{i}', true, true)
     }
+    // getTagType(item){
+    //   const tagTypeMap = new Map([
+    //     []
+    //   ])
+    // }
   }
 }
 </script>
